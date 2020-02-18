8f5dbd5c6294ed1c2b6a27846c12fc1b9043ecd6
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-18 16:54:07 -0700

    Added Functions to change bg and span to hex code on click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 const button = document.querySelector("button");
+const hexSpan = document.querySelector("span");
+const bg = document.querySelector("body");
 
 button.addEventListener("click", function() {
-  console.log("connected");
+  let hex = generateHexCode();
+  hexSpan.innerHTML = hex;
+  bg.style.backgroundColor = hex;
 });
+
+const generateHexCode = () => {
+  const variables = "0123456789ABCDEF";
+  let colorCode = "#";
+  for (i = 0; i < 6; i++) {
+    colorCode += variables[Math.floor(Math.random() * variables.length)];
+  }
+  return colorCode;
+};
